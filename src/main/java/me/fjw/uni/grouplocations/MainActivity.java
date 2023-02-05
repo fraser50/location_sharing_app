cf4faef496c066bcbdf27018adb664ebe8979c38
Fraser Watson <fraser.watson@mac.com>
2023-02-05 14:23:03 +0000

    Initial commit

---
+++ src/main/java/me/fjw/uni/grouplocations/MainActivity.java
@@ -0,0 +1,14 @@
+package me.fjw.uni.grouplocations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
