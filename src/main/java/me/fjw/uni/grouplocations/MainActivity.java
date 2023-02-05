80546cfa80c75d861e1d99760d8b85b8d08981f5
Fraser Watson <fraser.watson@mac.com>
2023-02-05 15:01:25 +0000

    Added WebView with JavaScript enabled

--- src/main/java/me/fjw/uni/grouplocations/MainActivity.java
+++ src/main/java/me/fjw/uni/grouplocations/MainActivity.java
@@ -2,13 +2,21 @@ package me.fjw.uni.grouplocations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView optionsView = findViewById(R.id.options);
+
+        optionsView.getSettings().setJavaScriptEnabled(true);
+        optionsView.loadUrl("file:///android_asset/web/listing/index.html");
     }
 }
