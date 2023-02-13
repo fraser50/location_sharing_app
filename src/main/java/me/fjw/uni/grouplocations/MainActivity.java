c30dee30930d4c94027a1603f9b7884f746ab89e
Fraser Watson <fraser.watson@mac.com>
2023-02-13 17:27:45 +0000

    Location of device is now shown on map (still need to centre display on marker)

--- src/main/java/me/fjw/uni/grouplocations/MainActivity.java
+++ src/main/java/me/fjw/uni/grouplocations/MainActivity.java
@@ -1,10 +1,19 @@
 package me.fjw.uni.grouplocations;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.location.LocationManagerCompat;
+import androidx.core.os.CancellationSignal;
+import androidx.core.util.Consumer;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.CookieManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
@@ -12,8 +21,13 @@ import android.webkit.WebView;
 public class MainActivity extends AppCompatActivity {
 
     public class SharedData {
+        public SharedData() {
+            setupRequestLocation();
+        }
         // This key is for testing on my local machine.
         private String authKey = "59aadb01555247e9ebcbf78c8ce3f6a08b696f0fe39e8861afbc371055b7db1b";
+        private double latitude = 0;
+        private double longitude = 0;
 
         @JavascriptInterface
         public String getAuthKey() {
@@ -33,6 +47,30 @@ public class MainActivity extends AppCompatActivity {
         @JavascriptInterface
         public void loadAuthKey() {
             CookieManager.getInstance().setCookie(getAPI(), "key=" + authKey);
+        }
+
+        @JavascriptInterface
+        public double getLatitude() {
+            return latitude;
+        }
+
+        @JavascriptInterface
+        public double getLongitude() {
+            return longitude;
+        }
+
+        // TODO: Actually check permissions
+        @SuppressLint("MissingPermission")
+        public void setupRequestLocation() {
+            LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            manager.requestLocationUpdates("gps", 1000, 10, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            });
         }
     }
 
@@ -56,5 +94,12 @@ public class MainActivity extends AppCompatActivity {
 
         optionsView.addJavascriptInterface(data, "sharedData");
         optionsView.loadUrl("file:///android_asset/web/listing/index.html");
+
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Log.d("providers", "Printing providers:");
+        for (String provider : manager.getAllProviders()) {
+            Log.d("providers", provider);
+        }
+
     }
 }
