e70f298737e3e0c7c01fb3a0516bee2d9a2e992e
Fraser Watson <fraser.watson@mac.com>
2023-02-26 20:33:40 +0000

    Added settings page files that weren't added last commit

---
+++ src/main/assets/web/listing/settings.js
@@ -0,0 +1,36 @@
+function generateStatus(name, state) {
+    var heading = document.createElement("h2");
+    heading.innerText = name + ": ";
+
+    var span = document.createElement("span");
+    span.innerText = state ? "Enabled" : "Disabled";
+    span.className = state ? "green" : "red";
+
+    heading.appendChild(span);
+
+    return heading;
+}
+
+function renderSettings(content) {
+    content.innerHTML = "";
+
+    content.appendChild(generateStatus("Location tracking", settings.isLocationServicesEnabled()));
+
+    var toggleButton = document.createElement("button");
+    toggleButton.innerText = "Toggle";
+    toggleButton.className = "optionButton";
+
+    toggleButton.onclick = function() {
+        settings.setLocationServicesEnabled(!settings.isLocationServicesEnabled());
+        renderSettings(content);
+    };
+
+    content.appendChild(toggleButton);
+
+    content.appendChild(generateStatus("Extended tracking"), settings.isExtendedTracking());
+
+    var description = document.createElement("h3");
+    description.innerText = "Extended tracking will only be enabled if you are participating in study 2. Study 2 involves tracking outside of the HW campus. The first option, location tracking, states whether your location on the campus may be tracked. Turning this off will stop the app from sending location updates to the server.";
+
+    content.appendChild(description);
+}
